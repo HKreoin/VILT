fdde58bb9a45b6d99637526890a0a29c7b9fb001
Kreoin <steamuser85@yandex.ru>
2024-10-25 09:50:26 +0300

    add avatar downloader

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,18 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         sleep(2);
-        //Validate
+
         $fields = $request->validate([
+            'avatar' => ['file', 'nullable', 'max:300'],
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed']
         ]);
+
+        if ($request->hasFile('avatar')) {
+            Storage::disk('public')->put('avatars', $request->avatar);
+        }
         //Register
         $user = User::create($fields);
         //Login
@@ -35,11 +41,22 @@ class AuthController extends Controller
         if (Auth::attempt($fields, $request->remember)) {
             $request->session()->regenerate();
  
-            return redirect()->intended('/');
+            return redirect()->intended('dashboard');
         }
  
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+ 
+        $request->session()->invalidate();
+     
+        $request->session()->regenerateToken();
+     
+        return redirect('/');
     }
 }
