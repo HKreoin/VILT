b1fef90edd2e4de5d008974a6b8112dec93f7d7f
Kreoin <steamuser85@yandex.ru>
2024-11-03 00:55:23 +0300

    tabs width from 4 to 2

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,54 +1,67 @@
 <template>
-    <div>
-        <header>
-            <nav>
-                <div class="space-x-6">
-                    <Link
-                        :href="route('home')"
-                        class="nav-link"
-                        :class="{ 'bg-slate-700' : $page.component === 'Home' }">
-                        Home
-                    </Link>
-                </div>
+  <div>
+    <header>
+      <nav>
+        <div class="space-x-6">
+          <Link
+            :href="route('home')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Home' }"
+          >
+            Home
+          </Link>
+        </div>
 
-                <div v-if="$page.props.auth.user" class="space-x-6 flex">
-                    <img class="avatar" :src="$page.props.auth.user.avatar? ('/storage/' + $page.props.auth.user.avatar) : '/storage/avatars/avatardefault.png'" alt=""/>
-                    <Link
-                        :href="route('dashboard')"
-                        class="nav-link"
-                        :class="{ 'bg-slate-700' : $page.component === 'Dashboard' }">
-                        Dashboard
-                    </Link>
+        <div v-if="$page.props.auth.user" class="space-x-6 flex">
+          <img
+            class="avatar"
+            :src="
+              $page.props.auth.user.avatar
+                ? '/storage/' + $page.props.auth.user.avatar
+                : '/storage/avatars/avatardefault.png'
+            "
+            alt=""
+          />
+          <Link
+            :href="route('dashboard')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Dashboard' }"
+          >
+            Dashboard
+          </Link>
 
-                    <Link
-                        :href="route('logout')"
-                        method="post"
-                        class="nav-link"
-                        as="button"
-                        type="button">
-                        Logout
-                    </Link>
-                </div>
-                <div v-else class="space-x-6">
-                    <Link
-                        :href="route('register')"
-                        class="nav-link"
-                        :class="{ 'bg-slate-700' : $page.component === 'Auth/Register' }">
-                        Register
-                     </Link>
+          <Link
+            :href="route('logout')"
+            method="post"
+            class="nav-link"
+            as="button"
+            type="button"
+          >
+            Logout
+          </Link>
+        </div>
+        <div v-else class="space-x-6">
+          <Link
+            :href="route('register')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Auth/Register' }"
+          >
+            Register
+          </Link>
 
-                    <Link
-                        :href="route('login')"
-                        class="nav-link"
-                        :class="{ 'bg-slate-700' : $page.component === 'Auth/Login'}">
-                        Login
-                    </Link>
-                </div>
-            </nav>
-        </header>
+          <Link
+            :href="route('login')"
+            class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Auth/Login' }"
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
+    </header>
 
-        <main class="p-4">
-            <slot />
-        </main>
-    </div>
+    <main class="p-4">
+      <slot />
+    </main>
+  </div>
 </template>
