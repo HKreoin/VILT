a49462ab61eede36351cc24b155f2983e58b6123
Kreoin <steamuser85@yandex.ru>
2024-11-02 23:21:20 +0300

    autoformat login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,19 +1,18 @@
 <script setup>
-
-import { useForm } from '@inertiajs/vue3';
-import TextInput from "../Components/TextInput.vue";
+import { useForm } from '@inertiajs/vue3'
+import TextInput from '../Components/TextInput.vue'
 
 const form = useForm({
     email: null,
     password: null,
     remember: null,
-});
+})
 
 const submit = () => {
-    form.post(route("login"), {
+    form.post(route('login'), {
         onError: () => form.reset('password'),
-    });
-};
+    })
+}
 </script>
 
 <template>
@@ -21,7 +20,6 @@ const submit = () => {
     <h1 class="title">Login to your account</h1>
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
-
             <TextInput
                 name="email"
                 type="email"
@@ -38,15 +36,20 @@ const submit = () => {
 
             <div class="flex items-center justify-between mb-2">
                 <div class="flex items-center gap-2">
-                    <input type="checkbox" v-model="form.remember">
+                    <input type="checkbox" v-model="form.remember" />
                     <label>Remember me</label>
                 </div>
-                
-                <p class="text-slate-600">Need an account? <a :href="route('register')" class="text-link">Register</a></p>
+
+                <p class="text-slate-600">
+                    Need an account?
+                    <a :href="route('register')" class="text-link">Register</a>
+                </p>
             </div>
 
             <div>
-                <button class="primary-btn" :disabled="form.processing">Login</button>
+                <button class="primary-btn" :disabled="form.processing">
+                    Login
+                </button>
             </div>
         </form>
     </div>
